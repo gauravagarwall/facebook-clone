d30275d8c6ed40b0e394978617c075e84ae85ed2
Gaurav <web.gaurav@agarwal.club>
2021-10-13 18:37:56 +0530

    Feed Completed

---
+++ src/Feed.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Feed.css";
+import MessageSender from "./MessageSender";
+
+function Feed() {
+  return (
+    <div className="feed">
+      {/* StoryReel */}
+      {/* MessageSender */}
+      <MessageSender />
+    </div>
+  );
+}
+
+export default Feed;
